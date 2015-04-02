de416569be6c95631a25d30b12117aedacebeee4
unknown <zdw1791@GDD-49.main.ad.rit.edu>
2015-04-02 19:39:41 -0400

    Player movement and camera movement is up

---
+++ Assets/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if(transform.position.x >= 1.3 &&)
+		{
+
+		}
+		transform.position = player.transform.position + offset;
+	
+	}
+}
---
+++ Assets/CollisionDetetection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDetetection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Player")
+		{
+			return;
+		}
+		other.rigidbody.velocity = new Vector3(0.0f,0.0f,0.0f);
+	}
+
+}
