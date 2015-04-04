a9c59335b3ff1300fcddf606ce1b7fe4019cf0f6
Jonathan Palmer <Jon@JonathanPalmerGD.com>
2015-04-04 13:33:32 -0400

    Pane stub code.

--- Assets/Scripts/Pane.cs
+++ Assets/Scripts/Pane.cs
@@ -6,6 +6,11 @@ public class Pane : MonoBehaviour
 	//Has a helper method for destroying itself
 	//When you call this, it fades out over time and then deletes itself from DotManager.
 
+	//They track the DotManager.Inst.activeDots.Count
+	//When happiness is beyond a certain point, begin destruction
+
+	//
+
 	public SpriteRenderer spriteRend;
 
 	void Start () 
